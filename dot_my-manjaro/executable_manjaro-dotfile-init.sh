5068d066858766c5227c3de47e07ea74d9cfb317
lolilijve <lolilijve@gmail.com>
2020-06-21 17:39:53 +0800

    --

--- dot_my-manjaro/executable_manjaro-dotfile-init.sh
+++ dot_my-manjaro/executable_manjaro-dotfile-init.sh
@@ -4,3 +4,12 @@ chezmoi diff
 
 # 安装 oh-my-fish -- Fishshell 框架
 curl -L https://get.oh-my.fish | fish
+
+# 配置 chromium -- http://googlehelper.net/（下载插件并安装如浏览器，同步即可）
+
+# 配置 idea -- （idea导入 ~/.my-manjaro/idea-settings/settings.zip）
+# idea会创建桌面快捷方式，在utools中显示两个应用，都可以打开，若有强迫症，可在/usr/share/applications/中删除（其他应用同理）
+
+
+
+
